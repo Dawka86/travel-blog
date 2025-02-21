aa5aa3d96e5c3e347cff958942043d1f9ff3ff0b
Dawid <105428376+Dawka86@users.noreply.github.com>
2025-02-21 18:18:50 +0000

    update

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-dark-100">
+      <h1 className="text-4xl font-bold text-center">Witaj na moim blogu podróżniczym!</h1>
+    </div>
+  );
+}
